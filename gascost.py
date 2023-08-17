1b00024e10d5e636a7292518ec46ae1a9df69f82
franoll fantu <124239428+franollf@users.noreply.github.com>
2023-08-16 21:33:53 -0700

    Update gascost.py

--- gascost.py
+++ gascost.py
@@ -1,7 +1,12 @@
-cartype = input("What car are you taking? Type Prius(P), Jaguar(J), Toyota(T), and Honda(H) ").lower()
+# Ask What Car Am I Using
+cartype = input("What car are you taking? Type Prius(P), Jaguar(J), Toyota(T), and Honda(H) ").upper()
+# Ask am I using the highway or travelling through the city?
 route = input("Are you taking the highway(H) or through the City(C) ").lower()
+# Ask where am I going?
 destination = input("Where are you going? ")
+# Ask How Far in KM am i going?
 distance = float(input("How far in KM are you driving? "))
+# Ask what is the price of Gas?
 price = float(input("What is the price of gas right now? "))
 
 literperkm = float(distance) * float(price)
@@ -16,28 +21,38 @@ hondah = 0.07
 jaguarc = 0.14
 jaguarh = 0.102267
 
+
+def main():
+    print(f" In the {cartype}, going to {destination} will cost {literperkm * priusc:.2f}$")
+
 if route == "c":
-    if cartype == "p": 
-     print(f" In the Prius, going to {destination} will cost {literperkm * priusc:.2f}$")
+    if cartype == "P": 
+        main()
     
-    if cartype == "j":
-        print(f" In the Jaguar, going to {destination} will cost {literperkm * jaguarc:.2f}$")
+    elif cartype == "J":
+        main()
 
-    if cartype == "t":
-        print(f" In the Toyota, going to {destination} will cost {literperkm * toyotac:.2f}$")
+    elif cartype == "T":
+        main()
 
-    if cartype == "h":
-        print(f" In the Honda, going to {destination} will cost {literperkm * hondac:.2f}$")
+    elif cartype == "H":
+        main()
+        
+    else:
+        print("Please enter a valid car type")
 
 if route == "h":
-    if cartype == "p":
-        print(f" In the Prius, going to {destination} will cost {literperkm * priush:.2f} $")
-    if cartype == "j":
-        print(f" In the Jaguar, going to {destination} will cost {literperkm * jaguarh:.2f} $")
+    if cartype == "P":
+        main()
+    elif cartype == "J":
+        main()
 
-    if cartype == "t":
-        print(f" In the Toyota, going to {destination} will cost {literperkm * toyotah:.2f} $")
+    elif cartype == "T":
+        main()
 
-    if cartype == "h":
-        print(f" In the Honda, going to {destination} will cost {literperkm * hondah:.2f}$")
-        
+    elif cartype == "H":
+        main()
+
+    else:
+        print("Please enter a valid car type")
+
