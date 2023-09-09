d77b851f9549e938d095a46aa7d5c9b4ca0bfaf5
franoll fantu <124239428+franollf@users.noreply.github.com>
2023-09-09 12:00:33 -0700

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,41 +1,56 @@
+
 import random
+
+
 
 p1_wins = 0
 p2_wins = 0
 
-for i in range(10):
 
-    p1_roll = random.randint(1,6)
-    p2_roll = random.randint(1,6)
+print("This is a Two Player Game")
 
-    print(f'Player 1 rolls: {p1_roll}')
-    print(f'PLayer 2 rolls: {p2_roll}')
+p1_name = input("\nPlayer 1: What is your name? ")
+p2_name = input("\nPlayer 2: What is your name? ")
 
-    if p1_roll > p2_roll:
-        print ('Player 1 Wins!')
+input("\nPress Enter to Play!")
+
+
+for _ in range(5):
+     
+     p1_roll = random.randint(1,6)
+     p2_roll = random.randint(1,6)
+
+     print(f"\n{p1_name} rolls {p1_roll}")
+     print(f"{p2_name} rolls {p2_roll}")
+
+     if p1_roll > p2_roll:
+        print("\nPlayer 1 Wins!")
         p1_wins += 1
-        
-
-    if p1_roll < p2_roll:
-        print ('Player 2 Wins!')
+    
+     if p2_roll > p1_roll:
+        print("\nPlayer 2 Wins!")
         p2_wins += 1
-        
 
-    if p1_roll == p2_roll:
-        print ('Draw!')
-        
+     elif p2_roll == p1_roll: 
+        print("\nIt was a tie!")
 
-    input('Press Enter To Continue:')
+     input("Press Enter to Continue")
 
-
-print("Game over!")
+  
 if p1_wins > p2_wins:
-    print(f'Player 1 Wins! {p1_wins}' ' points')
+    print(f"\n{p1_name} Wins by {p1_wins - p2_wins} win!")
 
-if p2_wins > p1_wins:
-    print(f'Player 2 Wins!{p2_wins}' 'points')  
+if p1_wins < p2_wins:
+    print(f"\n{p2_name} Wins by {p2_wins - p1_wins}!")
 
-        
+if p1_wins == p2_wins:
+    print(f"\n It was a tie! Nobody Wins!")
+    
+
+
+
+
+
 
 
 
